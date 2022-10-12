ed42e5f268114d937ad6b5c90a9e821547536224
Nanna Hansen <nann257j@stud.kea.dk>
2022-10-12 12:03:19 +0200

    cookieboks

--- src/main.js
+++ src/main.js
@@ -6,3 +6,19 @@ Alpine.start();
 
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+// COOKIE BOKS
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+if (sessionStorage.getItem("fjerncookieboks")) {
+  cookieboks.classList.add("hidden");
+}
